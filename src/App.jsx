14922566d9191edbf4462f1f572ae3b85ecac79d
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-17 20:00:05 -0300

    alteraçoes

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,12 @@ function App() {
 
     <div className="info">
 
-      <p>Tenho 24 anos e Sou apaixonado por piano,ui/ux e web</p>
-      <p>Cursando Análise e Desenv. de Sistemas na Estácio</p>
-      <p>DOMINIOS EM HTML ,CSS, JAVASCRIPT, TYPESCRIPT, REACT,GIT</p>
-      <p>Meu objetivo é  crescer na area utilizando React e typescript</p>
+      <div>
+        <p>Tenho 24 anos e Sou apaixonado por piano,ui/ux e web</p>
+        <p>Cursando <b>Análise e Desenv. de Sistemas</b> na <b>Estácio</b></p>
+        <p>DOMINIOS EM <span>HTML ,CSS, JAVASCRIPT, TYPESCRIPT, REACT,GIT</span></p>
+        <p>Meu objetivo é  crescer na area utilizando React e typescript</p>
+      </div>
 
       </div>
 
