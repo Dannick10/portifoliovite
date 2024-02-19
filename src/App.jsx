d57d9247316ddfd69a9bcb0e06c5b089bebcac82
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-19 19:06:47 -0300

    adicionando projetos

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ import NavSideBar from './components/NavSidebar'
 import Stacks from './components/Stacks'
 import Projects from './components/Projects'
 
+//imgs
+import jadoo from '/Project_img/jadoo_project.svg'
+import nossosol from '/Project_img/nossosol_project.svg'
+import educamais from '/Project_img/portal_educa.svg'
+import dogapi from '/Project_img/dog_api.svg'
+
 function App() {
   
   return (
@@ -43,12 +49,41 @@ function App() {
         <h2>PROJETOS</h2>
     </div>
 
-      <Projects stacks={<Stacks html={true} css={true} javascript={true} color='black'/>}/>
+      <Projects 
+      img={jadoo}
+      title='JADOO LADING PAGE'
+      info='Um portal de viagens que oferece uma experiência única Uma pagina atraente e responsiva.  Utilizando Html, Css,JavaScript,Git e duas bibliotecas vanilla-tilt.js e Notyf. Nesse projeto aprendi estruturar um ótimo HTML para conseguir manipular um site com muitos elementos.'
+      linksite='https://dannick10.github.io/jadoo_landing/'
+      linkgit='https://github.com/Dannick10/jadoo_landing'
+      stacks={<Stacks html={true} css={true} javascript={true} color='black'/>}
+     />
 
-      <Projects stacks={<Stacks html={true} css={true} javascript={true} color='white'/>}/>
+      <Projects 
+      img={nossosol}
+      title='NOSSO SOL'
+      info=' Um projeto real de conscientização para destacar a independência energética proporcionada pelos painéis solares. Esse projeto foi feito em colaboração com outro desenvolvedor utilizando o Git,React,vite e biblioteca swipe.js  todos os processos são separado em etapas.'
+      linksite='https://nosso-sol.vercel.app/'
+      linkgit='https://github.com/fabcode01/nosso-sol'
+      stacks={<Stacks react={true} html={true} css={true} javascript={true} git={true} color='white'/>}
+      />
 
-      
-      <Projects stacks={<Stacks html={true} css={true} javascript={true} color='black'/>}/>
+      <Projects 
+      img={educamais}
+      title='PORTAL EDUCA+'
+      info='Um projeto de educação mostrando principais cursos e apresentação da administração da instituição. A página é responsiva e tem uma experiência unica com efeitos de animações  Utilizando Html, Css,JavaScript,Git. Nesse projeto aprendi administrar e organizar meu tempo por ser parte de um desafio, do @FuturoDevJohn.'
+      linksite='https://dannick10.github.io/jadoo_landing/'
+      linkgit='https://github.com/Dannick10/jadoo_landing'
+      stacks={<Stacks html={true} css={true} javascript={true} color='black'/>}
+      />
+
+      <Projects 
+      img={dogapi}
+      title='DOG API'
+      info='Um projeto sobre raças de cachorros com informações explicativas de cada raças. A página é responsiva e minimalista. Utilizando HTML, CSS,JAVASCRIPT,GIT e a api  thedogapi. Esse projeto foi desafiador por eu ter consumido minha primeira API sozinho, Aprendi o uso do Fetch.'
+      linksite='https://dannick10.github.io/jadoo_landing/'
+      linkgit='https://github.com/Dannick10/jadoo_landing'
+      stacks={<Stacks html={true} css={true} javascript={true} color='white'/>}
+      />
 
 
   </section>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,23 @@
-import jadoo from '/Project_img/jadoo_project.svg'
-
-function Projects({stacks}) {
+function Projects({img,title,info,linksite,linkgit,stacks}) {
   return (
     <>
       <aside className='project_showcase'>
           <div className='top_project'>
 
               <div className='img_project'>
-                  <img src={jadoo} alt="" />
+                  <img src={img} alt="" />
               </div>
 
               <div className='description_project'>
-                  <h2>JADOO LANDING PAGE</h2>
-                 <p>Um portal de viagens que oferece uma experiência única Uma pagina atraente e responsiva.  Utilizando Html, Css,JavaScript,Git e duas bibliotecas vanilla-tilt.js e Notyf. Nesse projeto aprendi estruturar um ótimo HTML para conseguir manipular um site com muitos elementos.</p>
+                  <h2>{title}</h2>
+                 <p>{info}</p>
               </div>
           </div>
 
           <div className='stacks_project'>
                 <div className='buttons'>
-                  <a href="https://github.com/Dannick10/jadoo_landing" target='_blank'><button className='btn_p'>SITE</button></a>
-                  <a href="" target='_blank'><button className='btn_p'>GITHUB</button></a>
+                  <a href={linksite} target='_blank'><button className='btn_p'>SITE</button></a>
+                  <a href={linkgit} target='_blank'><button className='btn_p'>GITHUB</button></a>
                 </div>
 
                 <div>
