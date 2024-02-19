8ca3d295303c02d22058b50be24b129db3891185
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-19 17:30:54 -0300

    fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,3 @@
-import styles from './Projects.module.css'
 import jadoo from '/Project_img/jadoo_project.svg'
 
 function Projects({stacks}) {
@@ -7,20 +6,20 @@ function Projects({stacks}) {
       <aside className='project_showcase'>
           <div className='top_project'>
 
-              <div className={styles.img_project}>
+              <div className='img_project'>
                   <img src={jadoo} alt="" />
               </div>
 
-              <div className={styles.description_project}>
+              <div className='description_project'>
                   <h2>JADOO LANDING PAGE</h2>
                  <p>Um portal de viagens que oferece uma experiência única Uma pagina atraente e responsiva.  Utilizando Html, Css,JavaScript,Git e duas bibliotecas vanilla-tilt.js e Notyf. Nesse projeto aprendi estruturar um ótimo HTML para conseguir manipular um site com muitos elementos.</p>
               </div>
           </div>
 
-          <div className={styles.stacks_project}>
+          <div className='stacks_project'>
                 <div className='buttons'>
-                  <button className='btn_p'><a href="">SITE</a></button>
-                  <button className='btn_p'><a href="">GITHUB</a></button>
+                  <a href="https://github.com/Dannick10/jadoo_landing" target='_blank'><button className='btn_p'>SITE</button></a>
+                  <a href="" target='_blank'><button className='btn_p'>GITHUB</button></a>
                 </div>
 
                 <div>
