a8948f676478bcf9ba8570c6645945ccdf9554d6
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-02-21 16:27:12 -0300

    fontes e estilos

---
+++ src/components/Info_project.jsx
@@ -0,0 +1,24 @@
+import styles from './Info_projects.module.css'
+function Info_project({cordiv,fonte,fontwrite}) {
+  return (
+    <div>
+      <div className={styles.info_design}>
+        <div className={styles.colors}>
+
+            <div><h2 style={{fontFamily:`${fonte}`}}>CORES</h2></div>
+
+            <div className={styles.sub_info}>
+            <div  className={styles.color} style={{background:`${cordiv}`}}></div>
+                <p style={{fontFamily:`${fonte}`}}>{cordiv}</p>
+            </div>
+        </div>
+        <div className={styles.fonts}>
+            <h2 style={{fontFamily:`${fonte}`}}>FONTES</h2>
+                <p style={{fontFamily:`${fonte}`}}>{fontwrite}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Info_project
