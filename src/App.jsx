55be5ffd52008d9e231fb05c90373583e933c38b
unknown <danmaggot2@gmail.com>
2024-06-01 13:50:45 -0300

    info

--- src/App.jsx
+++ src/App.jsx
@@ -88,10 +88,10 @@ function App() {
     <div className="info">
 
       <div>
-        <p>Tenho 24 anos e Sou apaixonado por piano,ui/ux e web</p>
-        <p>Cursando <b>Análise e Desenv. de Sistemas</b> na <b>Estácio</b></p>
-        <p>DOMINIOS EM <span>HTML ,CSS, JAVASCRIPT, TYPESCRIPT, REACT, GIT, FIREBASE, REDUX</span></p>
-        <p>Meu objetivo é  crescer na area utilizando React</p>
+        <p>👨‍💻 Cursando <b>Análise e Desenvolvimento de Sistemas</b> pela <span>estácio</span>.</p>
+        <p>🖥️ Profissional com experiência em desenvolvimento <span>Frontend</span>, Com habilidades sólidas em diversas <b>tecnologias</b> e <b>frameworks</b>, desenvolvimento de interfaces de usuário elegantes e responsivas.</p>
+        <p>💾 Stacks: <span>HTML, CSS, Javascript, TypeScript, React, Redux, Redux tooolkit, Styled Components, Context API, Hooks, Git, Firebase</span></p>
+
       </div>
 
       </div>
