0f980b1f2a4f2a2cc67c4841742eb445a53c263d
unknown <danmaggot2@gmail.com>
2024-07-28 12:16:39 -0300

    fix removelinks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,21 +14,16 @@ const Footer = () => {
       <div className={styles.redes}>
         <ul>
           <li>
-            <a href="https://github.com/Dannick10" target="_blank">
-              <i className="fa-brands fa-github"></i> /Dannick10
-            </a>
+            <i className="fa-brands fa-github"></i>
+            <p>/Dannick10</p>
             <button data-url="https://github.com/Dannick10" onClick={copyText}>
               Copiar
             </button>
           </li>
 
           <li>
-            <a
-              href="https://www.linkedin.com/in/futurodevdaniel/"
-              target="_blank"
-            >
-              <i className="fa-brands fa-linkedin"></i> /futurodevdaniel
-            </a>
+            <i className="fa-brands fa-linkedin"></i>
+            <p>/futurodevdaniel</p>
             <button
               data-url="https://www.linkedin.com/in/futurodevdaniel/"
               onClick={copyText}
@@ -38,9 +33,8 @@ const Footer = () => {
           </li>
 
           <li>
-            <a href="https://maito:ykdanieldev@gmail.com" target="_blank">
-              <i className="fa-solid fa-envelope"></i> ykdanieldev@gmail.com
-            </a>
+            <i className="fa-solid fa-envelope"></i>
+            <p>ykdanieldev@gmail.com</p>
             <button data-url="ykdanieldev@gmail.com" onClick={copyText}>
               Copiar
             </button>
