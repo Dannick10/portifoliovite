1f7cf1fc784091fd25b88a3f3c282bedf396ad81
Dannick010 <danmaggot2@gmail.com>
2025-01-07 16:44:50 -0300

    fix: loading and bout style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,52 +3,73 @@ import "./Header.module.css";
 import styles from "./Header.module.css";
 import me from "/me.gif";
 import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 function Header() {
+  const [isPageLoaded, setIsPageLoaded] = useState(false);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsPageLoaded(true);
+      scroll(0,0)
+    }
+   
+    window.addEventListener("load", handleLoad);
+
+    return () => window.removeEventListener("load", handleLoad);
+  }, []);
+
   return (
     <>
       <header className={styles.header} id="header">
         <div className={styles.header_top}>
-          <motion.div
-            initial={{ x: -200, opacity: 0 }}
-            transition={{ duration: 0.6, type: 'tween' }}
-            animate={{ x: 0, opacity: 1 }}
-            viewport={{once: true}}
-          >
-            <h1>DANIEL</h1>
-          </motion.div>
+          {isPageLoaded && (
+            <>
+              <motion.div
+                initial={{ x: -200, opacity: 0 }}
+                transition={{ duration: 0.6, type: "tween" }}
+                animate={{ x: 0, opacity: 1 }}
+                viewport={{ once: true }}
+              >
+                <h1>DANIEL</h1>
+              </motion.div>
 
-          <div
-            className={styles.header_me}
-          
-          >
-            <motion.img src={me} alt="imagem do autor" 
-                initial={{ scale: 10, opacity: 0 }}
-                transition={{ duration: 0.8, type: 'spring' }}
-                animate={{ scale: 1, opacity: 1 }}
-                viewport={{once: true}}
-            />
-          </div>
-          <motion.div
-            initial={{ x: 200, opacity: 0 }}
-            transition={{ duration: 0.6, delay: 0.1,  type: 'tween' }}
-            animate={{ x: 0, opacity: 1 }}
-            viewport={{once: true}}
-          >
-            <h1>ROCHA</h1>
-          </motion.div>
+              <div className={`${styles.header_me}`}>
+                <motion.img
+                  src={me}
+                  alt="imagem do autor"
+                  initial={{ scale: 10, opacity: 0 }}
+                  transition={{ duration: 0.8, type: "spring" }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  viewport={{ once: true }}
+                />
+              </div>
+
+              <motion.div
+                initial={{ x: 200, opacity: 0 }}
+                transition={{ duration: 0.6, delay: 0.1, type: "tween" }}
+                animate={{ x: 0, opacity: 1 }}
+                viewport={{ once: true }}
+              >
+                <h1>ROCHA</h1>
+              </motion.div>
+            </>
+          )}
         </div>
 
-        <motion.div className={styles.header_bottom}
-         initial={{x:100, opacity: 0}}
-         transition={{duration: .7, delay: .3,  ease: 'circInOut'}}
-         animate={{x: 0, opacity: 1}}
-         viewport={{once: true}}
-        >
-          <h2>
-            <span>DEV.</span> FRONT END
-          </h2>
-        </motion.div>
+        {isPageLoaded && (
+          <motion.div
+            className={styles.header_bottom}
+            initial={{ x: 100, opacity: 0 }}
+            transition={{ duration: 0.7, delay: 0.3, ease: "circInOut" }}
+            animate={{ x: 0, opacity: 1 }}
+            viewport={{ once: true }}
+          >
+            <h2>
+              <span>DEV.</span> FULLSTACK
+            </h2>
+          </motion.div>
+        )}
       </header>
     </>
   );
